fa29d6da096953de012f5384dfb612d9e78ecaff
Your Name <you@example.com>
2025-05-12 10:11:34 +0600

    add porfolio img slider section

--- js/script.js
+++ js/script.js
@@ -19,3 +19,43 @@ resumeBtns.forEach((btn, idx) => {
         resumeDetails[idx].classList.add('active');
     });
 });
+
+
+const arrowRight = document.querySelector('.portfolio-box .navigation .arrow-right');
+const arrowLeft = document.querySelector('.portfolio-box .navigation .arrow-left');
+
+let index = 0;
+
+const activePortfolio = () => {
+    const imgSlide = document.querySelector('.portfolio-carousel .img-slide');
+
+    imgSlide.style.transform = `translateX(calc(${index * -100}% - ${index * 2}rem))`;
+}
+
+
+arrowRight.addEventListener('click', ()=>{
+
+    if (index < 4){
+        index++;
+    }
+    else{
+        index = 5;
+
+    }
+    activePortfolio();
+    
+}) 
+
+
+arrowLeft.addEventListener('click', ()=>{
+
+    if (index > 1){
+        index--;
+    }
+    else{
+        index = 0;
+
+    }
+    activePortfolio();
+    
+}) 
