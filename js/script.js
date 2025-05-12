52d119b0325f5f3bb788f5896ec830d98719becc
Your Name <you@example.com>
2025-05-12 18:43:55 +0600

    add responsive toggle bar

--- js/script.js
+++ js/script.js
@@ -1,11 +1,23 @@
 
 const navLinks = document.querySelectorAll('header nav a');
+const menuIcon = document.querySelector('#menu-icon');
+const navbar = document.querySelector('header nav');
+
+
+menuIcon.addEventListener('click', () => {
+    menuIcon.classList.toggle('bx-x');
+    navbar.classList.toggle('active');
+
+} );
 
 const activePage = () =>{
     navLinks.forEach(link => {
 
         link.classList.remove('active');
-    })
+    });
+
+     menuIcon.classList.toggle.remove('bx-x');
+    navbar.classList.toggle.remove('active');
 }
 
 navLinks.forEach((link, idx) => {
