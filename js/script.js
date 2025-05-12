1651661a54befa575c41956c185ca76b885e35cb
Your Name <you@example.com>
2025-05-12 12:44:29 +0600

    slider updated

--- js/script.js
+++ js/script.js
@@ -28,8 +28,15 @@ let index = 0;
 
 const activePortfolio = () => {
     const imgSlide = document.querySelector('.portfolio-carousel .img-slide');
+    const portfolioDetails = document.querySelectorAll('.portfolio-detail');
 
     imgSlide.style.transform = `translateX(calc(${index * -100}% - ${index * 2}rem))`;
+
+    portfolioDetails.forEach(detail =>{
+        detail.classList.remove('active');
+    });
+
+    portfolioDetails[index].classList.add('active');
 }
 
 
@@ -37,9 +44,11 @@ arrowRight.addEventListener('click', ()=>{
 
     if (index < 4){
         index++;
+        arrowLeft.classList.remove('disabled');
     }
     else{
         index = 5;
+        arrowRight.classList.add('disabled');
 
     }
     activePortfolio();
@@ -51,9 +60,11 @@ arrowLeft.addEventListener('click', ()=>{
 
     if (index > 1){
         index--;
+        arrowRight.classList.remove('disabled');
     }
     else{
         index = 0;
+        arrowLeft.classList.add('disabled');
 
     }
     activePortfolio();
